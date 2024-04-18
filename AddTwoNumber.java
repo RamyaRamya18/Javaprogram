c29771b67224991838463f3fd9f4a081253d9394
RamyaRamya18 <144212683+RamyaRamya18@users.noreply.github.com>
2024-04-18 16:01:19 +0530

    AddTwoNumber.java

--- AddTwoNumber.java
+++ AddTwoNumber.java
@@ -1,2 +1,16 @@
-package Javaprogram;public class AddTwoNumber {
+package Javaprogram;
+
+import java.util.Scanner;
+
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the a value :");
+        int a = scanner.nextInt();
+        System.out.println("Enter the b value :");
+        int b = scanner.nextInt();
+        int c = a + b;
+        System.out.println("print the c value :"+c);
+
+    }
 }
