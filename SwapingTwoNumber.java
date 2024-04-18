e6aeca4127373bcc316da8fde09a1d99613f243b
RamyaRamya18 <144212683+RamyaRamya18@users.noreply.github.com>
2024-04-18 16:02:45 +0530

    SwapingTwoNumber.java

--- SwapingTwoNumber.java
+++ SwapingTwoNumber.java
@@ -1,2 +1,18 @@
-package Javaprogram;public class SwapingTwoNumber {
+package Javaprogram;
+
+import java.util.Scanner;
+
+public class SwapingTwoNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a =");
+        int a = scanner.nextInt();
+        System.out.println("b =");
+        int b = scanner.nextInt();
+        int c = a;
+        a = b;
+        System.out.println("a ="+a);
+        b = c;
+        System.out.println("b ="+b);
+    }
 }
