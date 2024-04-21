e1e0a7cb43afc8e355b18da161ccaf6198641ce3
Ramyaselvaraj <ramyaselvaraj2155@gmail.com>
2024-04-21 20:32:18 +0530

    java program

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,2 +1,13 @@
-package Javaprogram;public class SquareRoot {
+package Javaprogram;
+
+import java.util.Scanner;
+public class SquareRoot {
+    public static void main(String[]args){
+        float a ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of a :");
+        a = scanner.nextFloat();
+        double result = Math.sqrt(a);
+        System.out.println("print the square root value :"+ result);
+    }
 }
--- TransportMatrix.java
+++ TransportMatrix.java
@@ -1,2 +1,21 @@
-package Javaprogram;public class TransportMatrix {
+package Javaprogram;
+
+public class TransportMatrix {
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int a[][] = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                a[j][i] = arr[i][j];
+            }
+        }
+        System.out.println("Transposed Matrix:");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
