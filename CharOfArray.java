beb8dee9a459465a469f35542f948587ea70486c
Ramyaselvaraj <ramyaselvaraj2155@gmail.com>
2024-04-25 17:04:49 +0530

    CharOfArray

---
+++ CharOfArray.java
@@ -0,0 +1,13 @@
+package Javaprogram;
+
+public class CharOfArray {
+    public static void main(String[] args) {
+        char []ch = {'j','a','v','a','c','o','d','e'};
+        for(int i=0;i< ch.length;i++){
+            System.out.print("print the char of array :");
+            System.out.println(ch[i]+" ");
+        }
+    }
+}
+
+
